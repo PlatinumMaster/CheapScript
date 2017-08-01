24feae1d9f1b4bc91cfc660173de9901c3e1cb37
PlatinumMaster <stevieskittleshax@gmail.com>
2017-08-01 11:35:53 +0000

    Add "clean" and "help" commands.
    Also simplified detection of the devkitARM path.

--- make_script.py
+++ make_script.py
@@ -1,54 +1,44 @@
 #!/usr/bin/env python3
 
-
 from glob import glob
-
 from pathlib import Path
-
 import os
 import itertools
 import subprocess
 import sys
-import platform
+import yaml
+import shutil
+
+''' devkitARM PATH detection. '''
 
 devkitPATH = ""
-
 Platform = platform.system()
-
 Paths = os.getenviron('PATH').split(';')
+
+with open("config.yml", r) as yamlfile:
+			config = yaml.load(ymlfile)
 
 for Path in Paths:
 	if "devkitARM" in Path:
 	   devkitPATH = Path
-	   print "devkitARM was detected at " + Path + "."
-	   break
+	   print "devkitARM was detected at " + devkitPATH + "."
+	   break  
 	else:
-		print "Hmm, devkitARM can't be found in your PATH.\n I will try looking in the default directory for your operating system."
-		if Platform == "Windows":
-			PATH = 'C://devkitPro//devkitARM//bin'		
-			if os.path.isdir(PATH) == False:
-				print "devkitARM cannot be found at the default location.\nPlease make sure it was properly installed and try again."
-				sys.exit(1)
-			else:
-				print "Found it."
-				
-		elif Platform == "Linux":
-			PATH = "/opt/devkitPro/devkitARM/bin/"
-			if os.path.isdir(PATH) == False:
-				print "devkitARM cannot be found at the default location.\nPlease make sure it was properly installed and try again."
-				sys.exit(1)
-			else:
-				print "Found it."
-				
+		print "devkitARM was not found in your PATH.\nChecking the configuration for a path to devkitARM..."
+		devkitPATH = config['general']['devkitARM_PATH']
+		if os.path.isdir(devkitPATH) == False:
+			print "The directory specified in your configuration file (" + devkitPATH + ") does not contain devkitARM.\nExiting..."
+	   	sys.exit(1)
 		else:
-			print("Unknown platform.")
-			sys.exit(1)
-
+			print "devkitARM was detected at " + devkitPATH + "."
+	   
+# Set up devkitARM for compilation.
 PREFIX = 'arm-none-eabi-'
 AS = (PATH + PREFIX + 'as')
-OBJCOPY = (PATH + PREFIX + 'objcopy')
-BUILD = './build'
 ASFLAGS = ['-mthumb']
+OBJCOPY = (PATH + PREFIX + 'objcopy')
+BUILD = config['general']['Build Folder']
+SOURCE = config['general']['Source Folder']
 
 def run_command(cmd):
 	try:
@@ -68,20 +58,43 @@ def objcopy(binary):
 	cmd = [OBJCOPY, '-O', 'binary', binary, binary.replace('.o', '.bin')]
 	run_command(cmd)
 
+def run_glob(globstr, fn):
+	'''Glob recursively and run the processor function on each file in result'''
+	if sys.version_info > (3, 4):
+		files = glob(os.path.join(SOURCE, globstr), recursive = True)
+		return map(fn, files)
+	else:
+		files = Path(SOURCE).glob(globstr)
+		return map(fn, map(str, files))
 
-def main():
-	# Create output directory
-	try:
-		os.makedirs(BUILD)
-	except FileExistsError:
-		pass
+def main(option):
+	if option == "build":
+		globs = {
+			'commands/B2W2.s': ignore,
+			'.s': process_assembly
+		}
+
+		try:
+			os.makedirs(BUILD)
+			
+		except FileExistsError:
+			pass
 		
-	# Process Assembly	
-	process_assembly(sys.argv(1))
+		# Process Assembly & Make Binary
+		objects = itertools.starmap(run_glob, globs.items())
+		objcopy(objects)
+		
+	elif option == "clean":
+		os.removedirs(BUILD)
+		print "Cleaned build directory."
+		
+	elif option == "help":
+		print "Help:" + "\n" + "make_script.py <syntax>" + "\n" + "\n" + "build - Attempt to build the source." + "\n" + "clean - Delete files from a previous build." + "\n" + "help - Displays the list of commands."
 
-	# Gather source files and process them
-	objcopy(sys.argv(1).replace(".s", ".o"))
+	else:
+		print "Unknown parameter. Run this program with the argument \"help\" to see the proper commands."
 
 if __name__ == '__main__':
-	main()
+	main(sys.argv[1].lower())
+    
     
