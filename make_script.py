f2031c5c978968e12f4332881ea229ae5082f219
PlatinumMaster <stevieskittleshax@gmail.com>
2017-08-02 17:47:38 +0000

    Add files via upload

--- make_script.py
+++ make_script.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+'''CheapScript Build System.
+Based on a similar one by DizzyEgg.
+©2017 PlatinumMaster.'''
+
 from glob import glob
 from pathlib import Path
 import os
@@ -10,24 +14,22 @@ import yaml
 import shutil
 
 ''' devkitARM PATH detection. '''
-
 devkitPATH = ""
-Platform = platform.system()
-Paths = os.getenviron('PATH').split(';')
+Paths = os.environ.get('PATH').split(';')
 
 with open("config.yml", r) as yamlfile:
-			config = yaml.load(ymlfile)
+			config = yaml.load(yamlfile)
 
 for Path in Paths:
 	if "devkitARM" in Path:
 	   devkitPATH = Path
-	   print "devkitARM was detected at " + devkitPATH + "."
+	   print ("devkitARM was detected at " + devkitPATH + ".")
 	   break  
 	else:
-		print "devkitARM was not found in your PATH.\nChecking the configuration for a path to devkitARM..."
-		devkitPATH = config['general']['devkitARM_PATH']
+		print ("devkitARM was not found in your PATH.\nChecking the configuration for a path to devkitARM...")
+		devkitPATH = config['general']['devkitARM PATH']
 		if os.path.isdir(devkitPATH) == False:
-			print "The directory specified in your configuration file (" + devkitPATH + ") does not contain devkitARM.\nExiting..."
+			print ("The directory specified in your configuration file (" + devkitPATH + ") does not contain devkitARM.\nExiting...")
 	   	sys.exit(1)
 		else:
 			print "devkitARM was detected at " + devkitPATH + "."
@@ -72,28 +74,26 @@ def main(option):
 		globs = {
 			'**/*.s': process_assembly
 		}
-
 		try:
 			os.makedirs(BUILD)
-			
 		except FileExistsError:
 			pass
-		
 		# Process Assembly & Make Binary
 		objects = itertools.starmap(run_glob, globs.items())
 		objcopy(objects)
 		
 	elif option == "clean":
 		shutil.rmtree(BUILD)
-		print "Cleaned build directory."
-		
+		print ("Cleaned build directory.")
 	elif option == "help":
-		print "Help:" + "\n" + "make_script.py <syntax>" + "\n" + "\n" + "build - Attempt to build the source." + "\n" + "clean - Delete files from a previous build." + "\n" + "help - Displays the list of commands."
-
+		print ("Help:" + "\n" + "make_script.py <syntax>" + "\n" + "\n" + "build - Attempt to build the source." + "\n" + "clean - Delete files from a previous build." + "\n" + "help - Displays the list of commands.")
+	elif option == "":
+		print ("No arguments given. Run \"help\" to see the possible arguments.")
 	else:
-		print "Unknown parameter. Run this program with the argument \"help\" to see the proper commands."
-
+		print ("Unknown parameter. Run this program with the argument \"help\" to see the possible arguments.")
+		
 if __name__ == '__main__':
 	main(sys.argv[1].lower())
+    
     
     
