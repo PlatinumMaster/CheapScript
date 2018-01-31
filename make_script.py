d570f4fb2e8a75342528e47dc2899ec278f3d493
PlatinumMaster <L33TGamer@outlook.com>
2018-01-30 21:44:14 -0500

    Added single script compilation.

--- make_script.py
+++ make_script.py
@@ -10,7 +10,7 @@ import sys
 import shutil
 
 # Usage
-usage = "CheapScript Build System\nUsage:\n\"help\" - Displays this, then exits.\n\"build\" - Builds all of the scripts in the scripts folder.\n\"clean\" - Cleans the output folder."
+usage = "CheapScript Build System\nUsage:\n\"help\" - Displays this, then exits.\n\"build\" - Builds all of the scripts in the scripts folder.\n\"clean\" - Cleans the output folder.\n\"exit\" - Exits this program."
 
 # devkitARM
 devkitPATH = 'C://devkitPro/devkitARM/bin/'
@@ -24,7 +24,7 @@ try:
 	os.makedirs('output')
 except FileExistsError:
 	pass
-			
+
 # Directory definitions.
 build_dir = os.getcwd() + "/output/"
 script_dir = os.getcwd() + "/scripts/"
@@ -35,26 +35,28 @@ def run_command(cmd):
 	except subprocess.CalledProcessError as e:
 		print(e.output.decode(), file = sys.stderr)
 		sys.exit(1)
-	
-def process_assembly():
-	scripts = os.listdir(script_dir)
+
+def batch_make_scripts():
+	scripts = glob.glob('scripts/*.s')
 	for script in scripts:
-		print("Assembling %s" % script)
-		cmd = [AS] + [ASFLAGS] + ['-c', script_dir + script, '-o', build_dir + script.replace('.s', '.o')]
-		run_command(cmd)
+		process_assembly(script)
 		
-def process_objects():
-	objects = os.listdir(build_dir)
+	objects = glob.glob("output/*.o")
 	for object in objects:
-		if object[-4:] == ".o":
-			pass
-		else:	
-			print("Making %s into binary" % object)
-			cmd = [OBJCOPY, '-O', 'binary', build_dir + object, build_dir + object.replace('.o', '.bin')]
-			run_command(cmd)
-			
+		process_object(object)
+
+def process_assembly(script):
+	print("Assembling %s" % script)
+	cmd = [AS] + [ASFLAGS] + ['-c', script, '-o', build_dir + os.path.splitext(os.path.basename(script))[0] + '.o']
+	run_command(cmd)
+
+def process_object(object):
+	print("Making %s into binary" % object)
+	cmd = [OBJCOPY, '-O', 'binary', object, build_dir + os.path.splitext(os.path.basename(object))[0] + '.bin']
+	run_command(cmd)
+
 def clean():
-	confirmation = input('Using this option will remove all compiled scripts from the output folder. Are you sure you want to continue? ')
+	confirmation = input('Using this option will remove all compiled scripts from the output folder. Are you sure you want to continue?\n')
 	if confirmation.lower() == "yes":
 		print("Cleaning build directory...")
 		shutil.rmtree(build_dir)
@@ -64,15 +66,29 @@ def clean():
 
 def main(choice):
 	if choice.lower() == "build":
-		process_assembly()
-		process_objects()
+		build_option = input('Choose option:\n1) Compile a script.\n2) Compile all scripts in the \"scripts\" folder.\n ')
+		if build_option.lower() == "1":
+			script_choice = input('Type the name of the script (no extension) you\'d like to compile. It must be in the script directory!\n')
+			process_assembly(script_dir + script_choice + '.s')
+			process_object(build_dir + script_choice + '.o')
+			print("Done!")
+			
+		elif build_option.lower() == "2":
+			batch_make_scripts()
+			print("Done!")
+		else:
+			print("Invalid choice.")
+
 	elif choice.lower() == "clean":
 		clean()
+	elif choice.lower() == "exit":
+		sys.exit(0)
 	else:
 		print("Unrecognized option.\n")
 		print(usage)
+		main(input('What would you like to do?\n'))
 try:
 	main(sys.argv[1])
 except IndexError:
-	print("No parameters given.\n")
 	print(usage)
+	main(input('What would you like to do?\n'))
